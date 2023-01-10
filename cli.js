bf4ad360f38937fc431d4e13a691c2f81bb98cda
Arpit078 <verma.arpit078@.com>
2023-01-11 03:41:05 +0530

    will do hash routing someday for now, it's good

--- cli.js
+++ cli.js
@@ -58,14 +58,20 @@ function about(){
 
 `;
 const loaderContent = `
+// this file is modular and imports other files.
+//import your components
 import about from "../components/about.js"
 import more from "../components/more.js"
+
+//load your components in the sessionStorage
 window.sessionStorage.setItem("about",about)
 window.sessionStorage.setItem("more",more)
 
-
+//write the homepage on index.html
 const dom = document.getElementById('virtual-dom');
 dom.innerHTML = about
+
+
 
 
 `;
@@ -136,7 +142,9 @@ const about = /*html*/`
 </div>
 
 `
-const aboutContent = `const about = /*html*/\`${about}\`\nexport default about`
+const aboutContent = `//use inline html vs code extension to write html like these in template strings. Don't forget write /*html*/ or /*css*/ 
+//before template string after installing inline html extention.
+const about = /*html*/\`${about}\`\nexport default about`
 
 
 const indexcssContent = `
@@ -223,6 +231,76 @@ const aboutcssContent = `
   }
 }
 `
+const readmeContent = `
+# ReactionJS 😁
+a JS framework that helps you make single page web apps, while saving you burn your head learning complex frameworks made for industries.
+# Installation and create app
+    $> npm install @arpit078/preactjs
+    $> npx @arpit078/preactjs
+# requirements for dev environment
+    - node and npm installed to run the cli script.
+    - VS code prefered.
+    - install inline html and live server extensions 
+    from vs code extension store.
+# Documentation
+
+## FOLDER STRUCTURE
+    ├───assets
+    ├───components
+    │   └───styles
+    ├───public
+    │   └───styles
+    └───scripts
+    -----x----------------x-----
+    1. assets : this folder is meant to contain all your asset files ,nothing more fancy.
+    
+    2. components : this is where all the view html js files and jsx files are to be written.
+     some examples have been already made inside the diretory. 
+     To make these some points to consider are:
+        
+        a. use template literals `` to write the html code in js file.
+        
+        b. use inline html vs code extention to make life easier.
+
+        c. don't forget to default export your const variable.
+    
+    3. public : this folder is used to host the index.html file and its stylesheet.
+    some points to remember about this file:
+        
+        a. index.html has a div with id virtual-dom that is the only place where you
+        are inserting your components jsx files.
+
+        b. along with virtual dom you can have the benefits of a real dom as you can 
+        that social media bar being in the real dom.
+
+        c. YES! you can still use document.querySelector and all dom manipulations 
+        with all your window api:}
+
+        d. do not change the order of script tags else somethings might not work.
+    
+    4. scripts : this folder is the heart of your app. Files in this folder include
+        
+        a. loader.js : this file loads your component into the sessionStorage for 
+        future use. whenever you make a new page just load in this file and you'll 
+        have it stored in the sessionStorage and available for view.
+
+        b. functional.js : this file is used to write all your custom functions
+        promoting you to write modular code, it really helps.
+
+        c. router.js : this is a very important file in your project. 
+        It handles all the routing on your single page application.
+
+            - to make a route first define a const that getsItem (your component)
+             from the sessionStorage.
+
+            - then add its relative link in the routes object.
+
+            - add the function similar to more() and about() that get triggered on
+             navigation to call onNavigate(pathname) inside.
+    
+
+
+`
 
 
 
@@ -235,7 +313,7 @@ try {
   fs.writeFileSync('./components/styles/about.css', aboutcssContent);
   fs.writeFileSync('./public/styles/index.css', indexcssContent);
   fs.writeFileSync('./public/index.html', indexhtmlContent);
-  fs.writeFileSync('./readme.md', "hello there this is the readme file.");
+  fs.writeFileSync('./readme.md', readmeContent);
 
 
   // file written successfully
---
+++ readme.md
@@ -0,0 +1,66 @@
+# ReactionJS 😁
+a JS framework that helps you make single page web apps, while saving you burn your head learning complex frameworks made for industries.
+# Installation and create app
+    $> npm install @arpit078/preactjs
+    $> npx @arpit078/preactjs
+# requirements for dev environment
+    - node and npm installed to run the cli script.
+    - VS code prefered.
+    - install inline html and live server extensions 
+    from vs code extension store.
+# Documentation
+
+## FOLDER STRUCTURE
+    ├───assets
+    ├───components
+    │   └───styles
+    ├───public
+    │   └───styles
+    └───scripts
+    -----x----------------x-----
+    1. assets : this folder is meant to contain all your asset files ,nothing more fancy.
+    
+    2. components : this is where all the view html js files and jsx files are to be written.
+     some examples have been already made inside the diretory. 
+     To make these some points to consider are:
+        
+        a. use template literals `` to write the html code in js file.
+        
+        b. use inline html vs code extention to make life easier.
+
+        c. don't forget to default export your const variable.
+    
+    3. public : this folder is used to host the index.html file and its stylesheet.
+    some points to remember about this file:
+        
+        a. index.html has a div with id virtual-dom that is the only place where you
+        are inserting your components jsx files.
+
+        b. along with virtual dom you can have the benefits of a real dom as you can 
+        that social media bar being in the real dom.
+
+        c. YES! you can still use document.querySelector and all dom manipulations 
+        with all your window api:}
+
+        d. do not change the order of script tags else somethings might not work.
+    
+    4. scripts : this folder is the heart of your app. Files in this folder include
+        
+        a. loader.js : this file loads your component into the sessionStorage for 
+        future use. whenever you make a new page just load in this file and you'll 
+        have it stored in the sessionStorage and available for view.
+
+        b. functional.js : this file is used to write all your custom functions
+        promoting you to write modular code, it really helps.
+
+        c. router.js : this is a very important file in your project. 
+        It handles all the routing on your single page application.
+
+            - to make a route first define a const that getsItem (your component)
+             from the sessionStorage.
+
+            - then add its relative link in the routes object.
+
+            - add the function similar to more() and about() that get triggered on
+             navigation to call onNavigate(pathname) inside.
+    
